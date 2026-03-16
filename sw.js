cd746ba83ff7fa4ddc0eb2e958d4773cfc695330
hefaro <155494605+hefaro@users.noreply.github.com>
2026-03-16 16:55:13 -0500

    Refactor service worker for caching and activation
    Updated service worker to use ASSETS constant for caching and added activation event for cleaning old caches.

--- sw.js
+++ sw.js
@@ -8,12 +8,34 @@ const ASSETS = [
   "./icono_Calculadora.png"
 ];
 
+// Instalación
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(assets))
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        // CORRECCIÓN: Usar ASSETS en mayúsculas
+        return cache.addAll(ASSETS);
+      })
+      .then(() => self.skipWaiting()) // Fuerza al SW a activarse apenas se instala
   );
 });
 
+// Activación (Limpieza de caché vieja)
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Estrategia de respuesta
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(res => res || fetch(e.request))
